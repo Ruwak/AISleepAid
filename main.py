b4ca74146c664991ede99305605d0100e0d90797
ytrewq64 <141670058+ytrewq64@users.noreply.github.com>
2026-01-23 12:36:27 -0800

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import os
-import pandas as pd
 
 
 # Global Variables:
